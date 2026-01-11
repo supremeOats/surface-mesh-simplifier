Fixed Q-matrix calculation

src/simplifier.py
@@ -34,7 +34,7 @@ def new_pos(v1, v2, Q1, Q2) -> np.ndarray:
         return Q_inv @ np.array([0, 0, 0, 1]).T
 
 def adjacent_faces(vert_index, mesh):
-    return mesh.faces[(mesh.faces == vert_index).all(axis=1)]
+    return mesh.faces[vert_index]
 
 def quadratic_error(pair, Q1, Q2):    
     v1 = np.append(pair[0], 1)
@@ -43,26 +43,38 @@ def quadratic_error(pair, Q1, Q2):
     v_hat = new_pos(v1, v2, Q1, Q2)
 
     return v_hat.T @ (Q1 + Q2) @ v_hat  # v'(Q1 + Q2)v
-    
-def q_matrix(vertex, faces):
-    return np.sum([
-        np.power(p.T @ vertex, 2)
-        for p in faces
-    ])
 
+def k_matrix(face, vertices):
+    v1, v2, v3 = vertices[face]
+    norm_vec = np.cross(v2-v1, v3-v1)       # a,b,c
+    norm_vec /= np.linalg.norm(norm_vec)    # normalize
+    
+    p = np.append(norm_vec, -np.dot(norm_vec, v1))
+    # ^ a.x1 + b.x1 + c.x1 + d = 0 =>
+    # d = -(a.x1 + b.x1 + c.x1) = -n.v1
+
+    return np.outer(p, p.T)
+    
+def q_matrix(vertex, faces, mesh):
+    Q = np.zeros((4,4))
+    for f in faces:
+        f = mesh.faces[f]
+        Q += k_matrix(f, mesh.vertices)
+        print(Q)
+    
 def simplify(mesh):
     q_matrices = np.zeros_like(mesh.vertices, dtype=np.ndarray)
 
     for i in range(len(mesh.vertices)):
         vert_faces = adjacent_faces(i, mesh)
-        q_matrices[i] = q_matrix(mesh.vertices[i], vert_faces)
+        q_matrices[i] = q_matrix(mesh.vertices[i], vert_faces, mesh)
     
     pair_costs = np.ndarray(2)
 
     for edge in mesh.edges:     # edge -> indices, pair -> positions
         pair = (mesh.vertices[edge[0]], mesh.vertices[edge[1]])
-        Q1 = q_matrix(pair[0], adjacent_faces(edge[0], mesh))
-        Q2 = q_matrix(pair[1], adjacent_faces(edge[1], mesh))
+        Q1 = q_matrix(pair[0], adjacent_faces(edge[0], mesh), mesh)
+        Q2 = q_matrix(pair[1], adjacent_faces(edge[1], mesh), mesh)
         
         heap = np.append(pair_costs, (quadratic_error(pair, Q1, Q2), edge))   # heap?
 
@@ -76,14 +88,14 @@ def simplify(mesh):
     ...
 
 #
-mesh_file = "./data/boat_model.stl"
+mesh_file = "./data/cube_triangulated.stl"
 
 mesh = trimesh.load_mesh(mesh_file)
 
-start = time.time()
-mesh = simplify(mesh)
-end = time.time()
+# start = time.time()
+# mesh = simplify(mesh)
+# end = time.time()
 
-mesh.export("./data/boat_modified.stl")
+# mesh.export("./data/cube_modified_1.stl")
 
-print(f"Time elapsed: {round(end - start, 3)}")
+# print(f"Time elapsed: {round(end - start, 3)}")