heap is fixed; error calculation seems to have a problem as models deform, TODO: check k_matrix and q_matrix; added basic user interface

src/simplifier.py
@@ -1,13 +1,24 @@
+import heapq
 import numpy as np
 from numpy.linalg import LinAlgError
 from numpy.typing import ArrayLike
-import trimesh
-import time
 
 def edge_collapse(edge, mesh) -> None:
+    """
+    :param edge: tuple of two indices of vertecies in the mesh
+    :param mesh: mesh object
+    """
+
     vertices = mesh.vertices
     faces = mesh.faces
     v_keep, v_kill = edge   # note: v_keep and v_kill are INDICES, not vertices
+
+    # print(f"v_keep: {vertices[v_keep]}")
+    # print(f"v_kill: {vertices[v_kill]}")
+
+    if (v_keep >= len(vertices) or v_kill >= len(vertices)
+            or any(vertices[v_keep]) == np.nan or any(vertices[v_kill]) == np.nan):
+        return None
 
     # new pos
     new_coor = (vertices[v_keep] + vertices[v_kill]) * 0.5
@@ -19,24 +30,35 @@ def edge_collapse(edge, mesh) -> None:
     faces[faces == v_kill] = v_keep
     vertices[v_kill] = np.nan
 
-    # degenerate_faces = [f for f in faces if len(np.unique(f, axis=0)) == 3]
-
     # update mesh
     mesh.update_faces(mesh.nondegenerate_faces())
     mesh.update_vertices(vert_to_keep)
 
 def new_pos(v1, v2, Q1, Q2) -> np.ndarray:
+    """
+    Docstring for new_pos
+    
+    :param v1: tuple of three floats
+    :param v2: tuple of three floats
+    :param Q1: Description
+    :param Q2: Description
+    :return: Description
+    :rtype: ndarray[_AnyShape, dtype[Any]]
+    """
+    
     try:
         Q_inv = np.linalg.inv(Q1 + Q2)
     except LinAlgError:
-        return np.amin([v1, v2, (v1+v2)*0.5])
+        #todo
+        return (v1+v2)*0.5
+    
     else:
         return Q_inv @ np.array([0, 0, 0, 1]).T
 
 def adjacent_faces(vert_index, mesh):
     return mesh.faces[vert_index]
 
-def quadratic_error(pair, Q1, Q2):    
+def quadratic_error(pair, Q1, Q2):
     v1 = np.append(pair[0], 1)
     v2 = np.append(pair[1], 1)
     
@@ -47,8 +69,9 @@ def quadratic_error(pair, Q1, Q2):
 def k_matrix(face, vertices):
     v1, v2, v3 = vertices[face]
     norm_vec = np.cross(v2-v1, v3-v1)       # a,b,c
-    norm_vec /= np.linalg.norm(norm_vec)    # normalize
-    
+    # norm_vec /= np.linalg.norm(norm_vec)    # normalize
+    #todo
+
     p = np.append(norm_vec, -np.dot(norm_vec, v1))
     # ^ a.x1 + b.x1 + c.x1 + d = 0 =>
     # d = -(a.x1 + b.x1 + c.x1) = -n.v1
@@ -60,42 +83,24 @@ def q_matrix(vertex, faces, mesh):
     for f in faces:
         f = mesh.faces[f]
         Q += k_matrix(f, mesh.vertices)
-        print(Q)
-    
-def simplify(mesh):
-    q_matrices = np.zeros_like(mesh.vertices, dtype=np.ndarray)
 
-    for i in range(len(mesh.vertices)):
-        vert_faces = adjacent_faces(i, mesh)
-        q_matrices[i] = q_matrix(mesh.vertices[i], vert_faces, mesh)
-    
-    pair_costs = np.ndarray(2)
+    return Q
+
+def simplify(mesh, iterations=0):
+    # pair_costs = np.ndarray(2)
+    heap = []
 
     for edge in mesh.edges:     # edge -> indices, pair -> positions
         pair = (mesh.vertices[edge[0]], mesh.vertices[edge[1]])
         Q1 = q_matrix(pair[0], adjacent_faces(edge[0], mesh), mesh)
         Q2 = q_matrix(pair[1], adjacent_faces(edge[1], mesh), mesh)
         
-        heap = np.append(pair_costs, (quadratic_error(pair, Q1, Q2), edge))   # heap?
-
-    # pair_costs = np.sort(pair_costs, axis=1)
-    pair_costs = sorted(pair_costs, reverse=True, key=lambda tup: tup[0])
-
-    for (cost, pair) in pair_costs:
-        edge_collapse(pair, mesh)
+        err = quadratic_error(pair, Q1, Q2)
+        
+        heapq.heappush(heap, (err, tuple(edge)))   # when tuple given heapq sorts by the first value
+    
+    for i in range(iterations):
+        edge_collapse(heap[i][1], mesh)
 
     return mesh
-    ...
 
-#
-mesh_file = "./data/cube_triangulated.stl"
-
-mesh = trimesh.load_mesh(mesh_file)
-
-# start = time.time()
-# mesh = simplify(mesh)
-# end = time.time()
-
-# mesh.export("./data/cube_modified_1.stl")
-
-# print(f"Time elapsed: {round(end - start, 3)}")